697a5e1557ca03d173ad6dbd96d73d51f59adedd
CCYA41 <121651206+CCYA41@users.noreply.github.com>
2023-03-14 09:33:41 +0900

    230312

--- 230310_4h/230310_4h.c
+++ 230310_4h/230310_4h.c
@@ -1,9 +1,23 @@
 ﻿#include<stdio.h>
+#include <limits.h>
 
+#define TRUE 1
+#define FALSE 0
+#define ERROR_CODE INT_MIN
+#define DATA_TYPE int
+//#define DATAA 5
+// int를 DATA로 정의
+typedef int DATA;
+// STL Standard Templete Library
+//enum DATA {
+//	DAAA = 5
+//};
 // Function Declaration
-void Push(int* const _pStack, int _stackSize, int* const _pIndex, int _data);
-void Print(int* _pStack, int _stackSize, int _index);
 
+void Push(int* const _pStack, int _stackSize, int* const _pIndex, int _data);
+int Pop(const int* const _pStack, int* const _pIndex);
+void Print(int* _pStack, int _stackSize, int _index);
+int CheckStackOverflow(int _stackSize, int _index);
 
 void main()
 {
@@ -12,6 +26,7 @@ void main()
 	int stack[5] = { 0 };
 	int stackSize = sizeof(stack) / sizeof(stack[0]);
 	int index = 0;
+	int pop = Pop(stack, &index);
 
 	//if (index < 5) {
 
@@ -19,14 +34,22 @@ void main()
 	//	++index;
 	//}
 
-	Push(stack, stackSize, &index, 5);
-	Print(stack, stackSize, index);
-	Push(stack, stackSize, &index, 3);
-	Print(stack, stackSize, index);
+	//Push(stack, stackSize, &index, 5);
+	//Print(stack, stackSize, index);
+	//Push(stack, stackSize, &index, 3);
+	//Print(stack, stackSize, index);
 
-	stack[index] = 3;
-	++index;
+	//stack[index] = 3;
+	//++index;
 
+
+	for (int i = 0; i < 5; ++i) {
+		Push(stack, stackSize, &index, i + 1);
+		Print(stack, stackSize, index);
+
+		printf("Pop: %d\n", Pop(stack, &index));
+		Print(stack, stackSize, index);
+	}
 }
 
 
@@ -50,26 +73,48 @@ void Push(int* const _pStack, int _stackSize, int* const _pIndex, int _data)
 	// ex) int const i => 변수의 값을 보호(자료형도 지켜야함)
 	// ex) const int const i => 변수와 자료형 둘다 다른형식으로 변경을 금지
 
-	if (*_pIndex < _stackSize) {
+	//if (*_pIndex < _stackSize)
+	if (!CheckStackOverflow(_stackSize, *_pIndex))
+	{
 		_pStack[*_pIndex] = _data;
 		++(*_pIndex);
 	}
 }
 
-
-void Print(int* _stack, int _stackSize, int _index)
+int Pop(const int* const _pStack, int* const _pIndex)
 {
+	if (_pStack == NULL || _pIndex == NULL)
+		return ERROR_CODE;
+
+	if (*_pIndex == 0) return ERROR_CODE;
+
+	int returnValue = _pStack[*_pIndex - 1];
+	--(*_pIndex);
+	return returnValue;
+}
+
+
+void Print(const int* const _pStack, int _stackSize, int _index)
+{
+
 	if (_index == 0) {
 		printf("Stack is empty!\n");
 		return;
 	}
 
 	for (int i = 0; i < _index; ++i) {
-		printf("%d - ", _stack[i]);
+		printf("%d - ", _pStack[i]);
 	}
 	printf("(%d)\n", _index);
 
 
 }
+
+int CheckStackOverflow(int _stackSize, int _index) {
+
+
+	return (_index >= _stackSize) ? TRUE : FALSE;
+}
+
 
 
